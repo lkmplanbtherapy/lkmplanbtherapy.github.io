2b2e1ab716753f33b2fb761fa58326c33364c09e
lkmplanbtherapy <lkmplanbtherapy@gmail.com>
2017-05-15 20:04:00 -0700

    Add files via upload

--- Assets/js/slider.js
+++ Assets/js/slider.js
@@ -2,6 +2,8 @@ window.onload = function() {
     if(!window.location.hash) {
         window.location = window.location + '#loaded';
         window.location.reload();
+		window.location.reload();
+		window.location.reload();
     }
 }
 $(document).ready(function(){
